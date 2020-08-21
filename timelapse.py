8e5c42a33f2f2084b76ad6595c845aac18a00701
Javier Lancha Vázquez <javier.lancha@gmail.com>
2020-08-21 18:50:10 +0200

    Initial commit

---
+++ timelapse.py
@@ -0,0 +1,42 @@
+import cv2
+import time
+import sys
+import subprocess
+
+from timeit import default_timer as timer
+
+class Timelapse():
+    def __init__(self, interval):
+        self.interval = interval
+
+    def start(self):
+        n = 0
+
+        while True:
+            start = timer()
+
+            self.camera = cv2.VideoCapture(0)
+            self.camera.set(3, 1920)
+            self.camera.set(4, 1080)
+            s, img = self.camera.read()
+            if s:
+                cv2.imwrite("pic" + str(n) + ".jpg", img)
+                n += 1
+
+            now = timer()
+            remaining = self.interval - (now - start)
+            if (remaining > 0):
+                if (remaining > 3):
+                    self.camera.release()
+
+                time.sleep(remaining)
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Error: Specify an interval for the timelapse")
+        exit()
+
+    interval = float(sys.argv[1])
+    timelapse_camera = Timelapse(interval)
+    timelapse_camera.start()
